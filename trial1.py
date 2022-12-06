e33fd3ad82fa0d003798ad76b101656ef3c5481e
vkt1414 <115020590+vkt1414@users.noreply.github.com>
2022-12-06 15:45:44 -0500

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -39,52 +39,46 @@ repos_df=pd.DataFrame.from_dict(repos_get_request_json)
 repos_list=repos_df['name'].tolist()
 repos_list
 
-for repo in repos_list:
+#for repo in repos_list:
   #Clones_traffic
-  for i in range(0,50):
+  #stargazers
+for i in range(0,50):
+  #try:
+    repo='IDC-WebApp'
+    #get request for list of people that have starred the repository
+    #documentation:https://docs.github.com/en/rest/activity/starring?apiVersion=2022-11-28#list-stargazers
+    star_gazers_get_request='https://api.github.com/repos/ImagingDataCommons/'+repo+'/stargazers'
+    #converting api response to json using authenticated session
     try:
-      #repo='IDC-WebApp'
-      #get request for clone traffic
-      #documentation:https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-repository-clones
-      clone_traffic_get_request= 'https://api.github.com/repos/ImagingDataCommons/'+repo+'/traffic/clones'
-      #converting api response to json using authenticated session
-      try:
-        clone_traffic_get_request_json = gh_session.get(clone_traffic_get_request).json()
-        print (str(datetime.now())+' '+repo+' ' +'authentication successful while requesting clone traffic')
-      except:
-        print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ') 
-      #converting json to pandas dataframe
-      clone_count_df=pd.DataFrame()
-      clone_count_df=pd.json_normalize(clone_traffic_get_request_json, record_path =['clones'])
-      #adding a column to indicate the repo_name,adding timestamp, and renaming columns, converting timestamp to datetime64
-      clone_count_df['repo']=repo
-      clone_count_df['timestamp_data_pulled'] = pd.to_datetime('today')
-      clone_count_df=clone_count_df.rename(columns={'count':'clone_count', 'uniques':'unique_clone_count'})
-      clone_count_df['timestamp']= pd.to_datetime(clone_count_df['timestamp']).dt.tz_localize(None)
-      #getting clone counts only from the day before
-      #clone_count_df=clone_count_df[clone_count_df['timestamp']==(datetime.now()- timedelta(days=1)).strftime("%Y-%m-%d")]
-      print(str(datetime.now())+' '+repo+' ' +'successfully retreived clone counts into clone_count_df')
-      clone_count_df_appended=pd.concat([clone_count_df_appended,clone_count_df])
-      break
+      star_gazers_json = gh_session.get(star_gazers_get_request).json()
+      print (str(datetime.now())+' '+repo+' ' +'authentication successful while requesting for list of people that have starred the repository')
     except:
-      print(str(datetime.now())+' '+repo+' ' +'attempt to retreive clone traffic was unsuccessful, check for errors while converting json response to dataframe/n')
-      print('retrying')
-      continue
+      print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ')
+    #converting json to pandas dataframe
+    star_gazers_df=pd.DataFrame() 
+    star_gazers_df= pd.json_normalize(star_gazers_json)
+    #adding a column to indicate the repo_name, and a timestamp
+    star_gazers_df['repo']=repo
+    star_gazers_df['timestamp_data_pulled'] = pd.to_datetime('today')
+    #selecting only a few columns
+    star_gazers_df=star_gazers_df[['login', 'html_url','type', 'site_admin', 'repo','timestamp_data_pulled']]
+    star_gazers_df_appended=pd.concat([star_gazers_df_appended,star_gazers_df]) 
+    print(str(datetime.now())+' '+repo+' ' +'successfully retreived the list of stargazers into star_gazers_df')
+#    break
+#   except:
+#     print(str(datetime.now())+' '+repo+' ' +'attempt to retreive stargazers was unsuccessful, check for errors while converting json response to dataframe/n') 
+#     print('retrying')
+#     continue
 
-  for i in range(0,5):
-#    try:
-      #setting schema
-      clone_count_df_job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField("timestamp", bigquery.enums.SqlTypeNames.TIMESTAMP),
-                                                  bigquery.SchemaField("repo", bigquery.enums.SqlTypeNames.STRING),
-                                                  bigquery.SchemaField("clone_count", bigquery.enums.SqlTypeNames.INT64),
-                                                  bigquery.SchemaField("unique_clone_count", bigquery.enums.SqlTypeNames.INT64),
-                                                  bigquery.SchemaField("timestamp_data_pulled", bigquery.enums.SqlTypeNames.TIMESTAMP),
-                                                  ])
-      #loading into bq
-      clone_count_df_job = client.load_table_from_dataframe(clone_count_df_appended, "idc-external-025.logs.gh_clone_count", job_config=clone_count_df_job_config)
-      print('successfully loaded data from clone_count_df_appended dataframe to bigquery')
-#      break
-#    except:
- #     print('loading data from clone_count_df_appended dataframe to bigquery was unsuccessful/n')
- #     print('retrying to load into bigquery')
-  #    continue
+# for i in range(0,5):
+#   try:
+#     #not setting schema as there are many columns
+#     #star_gazers_df_job_config = []
+#     #loading into bq
+#     star_gazers_df_job = client.load_table_from_dataframe(star_gazers_df_appended, "idc-external-025.logs.gh_star_gazers") 
+#     print('successfully loaded data from star_gazers_df_appended dataframe to bigquery')
+#     break
+#   except:
+#     print('loading data from star_gazers_df_appended dataframe to bigquery was unsuccessful/n')
+#     print('retrying to load into bigquery')
+#     continue
