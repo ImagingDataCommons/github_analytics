54f953531a66974a6a38cb4969c29a05f14ffd62
vkt1414 <115020590+vkt1414@users.noreply.github.com>
2022-12-07 14:45:52 -0500

    updated logic

--- gh_insights_etl.py
+++ gh_insights_etl.py
@@ -69,7 +69,7 @@ for repo in repos_list:
         clone_count_df=clone_count_df.rename(columns={'count':'clone_count', 'uniques':'unique_clone_count'})
         clone_count_df['timestamp']= pd.to_datetime(clone_count_df['timestamp']).dt.tz_localize(None)
         #getting clone counts only from the day before
-        #clone_count_df=clone_count_df[clone_count_df['timestamp']==(datetime.now()- timedelta(days=1)).strftime("%Y-%m-%d")]
+        clone_count_df=clone_count_df[clone_count_df['timestamp']==(datetime.now()- timedelta(days=1)).strftime("%Y-%m-%d")]
         print(str(datetime.now())+' '+repo+' ' +'successfully retreived clone counts into clone_count_df')
         clone_count_df_appended=pd.concat([clone_count_df_appended,clone_count_df])
         break
@@ -111,7 +111,7 @@ for repo in repos_list:
       #converting api response to json using authenticated session
       try:
         views_traffic_json= gh_session.get(view_traffic_get_request).json()
-        print (str(datetime.now())+' '+repo+' ' +'authentication successful while requesting clone traffic')
+        print (str(datetime.now())+' '+repo+' ' +'authentication successful while requesting views traffic')
       except:
         print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ') 
       #converting json to pandas dataframe
@@ -124,7 +124,7 @@ for repo in repos_list:
         view_count_df=view_count_df.rename(columns={'count':'views_count', 'uniques':'unique_view_count'})
         view_count_df['timestamp']= pd.to_datetime(view_count_df['timestamp']).dt.tz_localize(None)
         #getting view counts only from the day before
-        #view_count_df=view_count_df[view_count_df['timestamp']==(datetime.now()- timedelta(days=1)).strftime("%Y-%m-%d")]
+        view_count_df=view_count_df[view_count_df['timestamp']==(datetime.now()- timedelta(days=1)).strftime("%Y-%m-%d")]
         view_count_df_appended=pd.concat([view_count_df_appended,view_count_df])
         print(str(datetime.now())+' '+repo+' ' +'successfully retreived view counts into view_count_df')
         break
@@ -259,8 +259,11 @@ for i in range(0,5):
     print('retrying to load into bigquery')
     continue      
 
-  #stargazers
+#stargazers
+yesterday=(datetime.now()- timedelta(days=1)).strftime("%Y-%m-%d")
+
 for repo in repos_list:
+  #stargazers
   for i in range(0,50):
     try:
       #repo='ai_medima_misc '
@@ -269,7 +272,7 @@ for repo in repos_list:
       star_gazers_get_request='https://api.github.com/repos/ImagingDataCommons/'+repo+'/stargazers'
       #converting api response to json using authenticated session
       try:
-        star_gazers_json = gh_session.get(star_gazers_get_request).json()
+        star_gazers_json = gh_session.get(star_gazers_get_request,headers ={'accept':'application/vnd.github.v3.star+json'}).json()
         print (str(datetime.now())+' '+repo+' ' +'authentication successful while requesting for list of people that have starred the repository')
       except:
         print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ')
@@ -280,8 +283,10 @@ for repo in repos_list:
       star_gazers_df['repo']=repo
       star_gazers_df['timestamp_data_pulled'] = pd.to_datetime('today')
       #selecting only a few columns
-      if 'login' in star_gazers_df.columns:
-        star_gazers_df=star_gazers_df[['login', 'html_url','type', 'site_admin', 'repo','timestamp_data_pulled']]
+      if 'user.login' in star_gazers_df.columns:
+        star_gazers_df=star_gazers_df[['starred_at','user.login', 'user.html_url','user.type', 'user.site_admin', 'repo','timestamp_data_pulled']]
+        star_gazers_df.columns = star_gazers_df.columns.str.replace(r".", "_", regex=True)
+        star_gazers_df=star_gazers_df[star_gazers_df['starred_at']>yesterday]
         star_gazers_df_appended=pd.concat([star_gazers_df_appended,star_gazers_df]) 
         print(str(datetime.now())+' '+repo+' ' +'successfully retreived the list of stargazers into star_gazers_df')
       else:
@@ -306,57 +311,12 @@ for i in range(0,5):
     print('retrying to load into bigquery')
     continue
 
-#subscribers
-for repo in repos_list:
-  for i in range(0,50):
-    try:
-      #repo='IDC-WebApp'
-      #get request for list of subscribers of the repository
-      #documentation: https://docs.github.com/en/rest/activity/watching?apiVersion=2022-11-28#list-watchers
-      subscribers_get_request='https://api.github.com/repos/ImagingDataCommons/'+repo+'/subscribers'
-      #converting api response to json using authenticated session
-      try:
-        subscribers_json = gh_session.get(subscribers_get_request).json()
-        print(str(datetime.now())+' '+repo+' ' +'authentication successful while requesting list of subscribersof the repository')
-      except:
-        print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ')
-      #converting json to pandas dataframe
-      subscribers_df=pd.DataFrame()  
-      subscribers_df= pd.json_normalize(subscribers_json)
-      #adding a column to indicate the repo_name
-      subscribers_df['repo']=repo
-      subscribers_df['timestamp_data_pulled'] = pd.to_datetime('today')
-      #selecting only a few columns
-      if 'login' in subscribers_df.columns:
-        subscribers_df=subscribers_df[['login', 'html_url','type', 'site_admin', 'repo','timestamp_data_pulled']]
-        subscribers_df_appended=pd.concat([subscribers_df_appended,subscribers_df])
-        print(str(datetime.now())+' '+repo+' ' +'successfully retreived  list of subscribers into subscribers_df')
-      else:
-        print('skipped converting api response to dataframe')
-        break
-      break    
-    except:
-      print(str(datetime.now())+' '+repo+' ' +'attempt to retreive subscribers was unsuccessful, check for errors while converting json response to dataframe/n') 
-      print('retrying')
-      continue
-      
-for i in range(0,5):
-  try:
-    #not setting schema as there are many columns
-    #loading into bq
-    subscribers_df_job = client.load_table_from_dataframe(subscribers_df_appended, "idc-external-025.logs.gh_subscribers") 
-    print('successfully loaded data from subscribers_df_appended dataframe to bigquery')
-    break
-  except:
-    print('loading data from subscribers_df_appended dataframe to bigquery was unsuccessful/n')
-    print('retrying to load into bigquery')
-    continue
-
 
 #contributor_commit_activity
-for repo in repos_list:
-  if findDay(now)== 'Wednesday':
-    latest_week_sunday=(datetime.now()- timedelta(days=3)).strftime("%Y-%m-%d")
+if findDay(now)== 'Monday':
+  for repo in repos_list:
+
+    latest_week_sunday=(datetime.now()- timedelta(days=1)).strftime("%Y-%m-%d")
 
     for i in range(0,50):
       try:
@@ -396,111 +356,117 @@ for repo in repos_list:
         print('retrying')
         continue
 
-for i in range(0,5):
-  try:
-    #not setting schema as there are many columns
-    #commits_df_job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField("days", bigquery.enums.SqlTypeNames.STRING), ])
-
-    #loading into bq
-    contributor_commit_activity_df_job = client.load_table_from_dataframe(contributor_commit_activity_df_appended, "idc-external-025.logs.gh_contributor_commit_activity") 
-    print('successfully loaded data from contributor_commit_activity_df_appended dataframe to bigquery')
-    break
-  except:
-    print('loading data from contributor_commit_activity_df_appended dataframe to bigquery was unsuccessful/n')
-    print('retrying to load into bigquery')
-    continue
+  for i in range(0,5):
+    try:
+      #not setting schema as there are many columns
+      #commits_df_job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField("days", bigquery.enums.SqlTypeNames.STRING), ])
+                                                
+      #loading into bq
+      contributor_commit_activity_df_job = client.load_table_from_dataframe(contributor_commit_activity_df_appended, "idc-external-025.logs.gh_contributor_commit_activity") 
+      print('successfully loaded data from contributor_commit_activity_df_appended dataframe to bigquery')
+      break
+    except:
+      print('loading data from contributor_commit_activity_df_appended dataframe to bigquery was unsuccessful/n')
+      print('retrying to load into bigquery')
+      continue
 
 #list_repository_languages
-for repo in repos_list:
-  for i in range(0,50):
-    try:
-      #repo='IDC-WebApp'
-      #get request for a list of languages for the specified repository. The value shown for each language is the number of bytes of code written in that language
-      #documentation: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-languages
-      list_repository_languages= 'https://api.github.com/repos/ImagingDataCommons/'+repo+'/languages'
-      #converting api response to json using authenticated session
+if findDay(now)== 'Monday':
+  for repo in repos_list:
+    for i in range(0,50):
       try:
-        list_repository_languages_json = gh_session.get(list_repository_languages).json()
-        print(str(datetime.now())+' '+repo+' ' +'authentication successful while requesting list of languages for the specified repository')
+        #repo='IDC-WebApp'
+        #get request for a list of languages for the specified repository. The value shown for each language is the number of bytes of code written in that language
+        #documentation: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-languages
+        list_repository_languages= 'https://api.github.com/repos/ImagingDataCommons/'+repo+'/languages'
+        #converting api response to json using authenticated session
+        try:
+          list_repository_languages_json = gh_session.get(list_repository_languages).json()
+          print(str(datetime.now())+' '+repo+' ' +'authentication successful while requesting list of languages for the specified repository')
+        except:
+          print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ')
+        #converting json to pandas dataframe
+        list_repository_languages_df=pd.DataFrame()  
+        list_repository_languages_df=pd.json_normalize(list_repository_languages_json)
+        #adding a column to indicate the repo_name 
+        list_repository_languages_df['repo']=repo
+        #adding timestamp of when the data was pulled
+        list_repository_languages_df['timestamp_data_pulled'] = pd.to_datetime('today')
+        list_repository_languages_df_appended=pd.concat([list_repository_languages_df_appended, list_repository_languages_df])
+        print(str(datetime.now())+' '+repo+' ' +'successfully retreived list of repository languages into list_repository_languages_df')
+        break 
       except:
-        print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ')
-      #converting json to pandas dataframe
-      list_repository_languages_df=pd.DataFrame()  
-      list_repository_languages_df=pd.json_normalize(list_repository_languages_json)
-      #adding a column to indicate the repo_name 
-      list_repository_languages_df['repo']=repo
-      #adding timestamp of when the data was pulled
-      list_repository_languages_df['timestamp_data_pulled'] = pd.to_datetime('today')
-      list_repository_languages_df_appended=pd.concat([list_repository_languages_df_appended, list_repository_languages_df])
-      print(str(datetime.now())+' '+repo+' ' +'successfully retreived list of repository languages into list_repository_languages_df')
-      break 
+        print(str(datetime.now())+' '+repo+' ' +'attempt to retreive a list of repository languages was unsuccessful, check for errors while converting json response to dataframe/n')
+        print('retrying')
+        continue
+
+
+  for i in range(0,5):
+    try:
+      #not setting schema as there are many columns
+      #commits_df_job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField("days", bigquery.enums.SqlTypeNames.STRING), ])
+                                                
+      #loading into bq
+      list_repository_languages_df_appended.columns = list_repository_languages_df_appended.columns.str.replace(r" ", "_", regex=True)
+      list_repository_languages_df_job = client.load_table_from_dataframe(list_repository_languages_df_appended, "idc-external-025.logs.gh_repository_languages") 
+      print('successfully loaded data from list_repository_languages_df_appended dataframe to bigquery')
+      break
     except:
-      print(str(datetime.now())+' '+repo+' ' +'attempt to retreive a list of repository languages was unsuccessful, check for errors while converting json response to dataframe/n')
-      print('retrying')
+      print('loading data from list_repository_languages_df_appended dataframe to bigquery was unsuccessful/n')
+      print('retrying to load into bigquery')
       continue
-
-for i in range(0,5):
-  try:
-    #not setting schema as there are many columns
-    #commits_df_job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField("days", bigquery.enums.SqlTypeNames.STRING), ])
-
-    #loading into bq
-    list_repository_languages_df_appended.columns = list_repository_languages_df_appended.columns.str.replace(r" ", "_", regex=True)
-    list_repository_languages_df_job = client.load_table_from_dataframe(list_repository_languages_df_appended, "idc-external-025.logs.gh_repository_languages") 
-    print('successfully loaded data from list_repository_languages_df_appended dataframe to bigquery')
-    break
-  except:
-    print('loading data from list_repository_languages_df_appended dataframe to bigquery was unsuccessful/n')
-    print('retrying to load into bigquery')
-    continue
 
 #forks
-for repo in repos_list:
-  for i in range(0,50):
-    try:
-      #repo='IDC-WebApp'
-      #get request for list of the forks of the repository. lists only the forks originated from the repository and does not list complete fork history. 
-      #i.e the current repository may itself be a fork of some other repository
-      #documentation: https://docs.github.com/en/rest/repos/forks?apiVersion=2022-11-28#list-forks
-      forks= 'https://api.github.com/repos/ImagingDataCommons/'+repo+'/forks'
-      #converting api response to json using authenticated session
+if findDay(now)== 'Monday':
+  for repo in repos_list:
+    current_week_monday=(datetime.now()).strftime("%Y-%m-%d")
+    last_week_monday=(datetime.now()- timedelta(days=7)).strftime("%Y-%m-%d")
+    for i in range(0,50):
       try:
-        forks_json = gh_session.get(forks).json()
-        print(str(datetime.now())+' '+repo+' ' +'authentication successful while requesting list of languages for the specified repository')
+        #repo='IDC-WebApp'
+        #get request for list of the forks of the repository. lists only the forks originated from the repository and does not list complete fork history. 
+        #i.e the current repository may itself be a fork of some other repository
+        #documentation: https://docs.github.com/en/rest/repos/forks?apiVersion=2022-11-28#list-forks
+        forks= 'https://api.github.com/repos/ImagingDataCommons/'+repo+'/forks'
+        #converting api response to json using authenticated session
+        try:
+          forks_json = gh_session.get(forks).json()
+          print(str(datetime.now())+' '+repo+' ' +'authentication successful while requesting list of languages for the specified repository')
+        except:
+          print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ') 
+        #converting json to pandas dataframe
+        forks_df=pd.DataFrame() 
+        forks_df=pd.json_normalize(forks_json)
+        #adding a column to indicate the repo_name 
+        forks_df['repo']=repo
+        #adding timestamp of when the data was pulled
+        forks_df['timestamp_data_pulled'] = pd.to_datetime('today')
+        #replacing columns with . to _
+        if 'full_name' in forks_df.columns:
+          forks_df.columns = forks_df.columns.str.replace(r".", "_", regex=True)
+          forks_df=forks_df[['full_name','private','html_url','fork','created_at','owner_login','owner_html_url','repo','timestamp_data_pulled']]
+          #forks_df=forks_df[(forks_df['created_at']>= last_week_monday) & (forks_df['created_at']< current_week_monday)]
+          forks_df_appended=pd.concat([forks_df_appended,forks_df])
+          print(str(datetime.now())+' '+repo+' ' +'successfully retreived list of forks into forks_df')
+        else:
+            print('skipped converting api response to dataframe')
+            break
+        break     
       except:
-        print(str(datetime.now())+' '+repo+' ' +'authentication unsuccessful,perhaps a time out error ') 
-      #converting json to pandas dataframe
-      forks_df=pd.DataFrame() 
-      forks_df=pd.json_normalize(forks_json)
-      #adding a column to indicate the repo_name 
-      forks_df['repo']=repo
-      #adding timestamp of when the data was pulled
-      forks_df['timestamp_data_pulled'] = pd.to_datetime('today')
-      #replacing columns with . to _
-      if 'full_name' in forks_df.columns:
-        forks_df.columns = forks_df.columns.str.replace(r".", "_", regex=True)
-        forks_df=forks_df[['full_name','private','html_url','fork','created_at','owner_login','owner_html_url','repo','timestamp_data_pulled']]
-        forks_df_appended=pd.concat([forks_df_appended,forks_df])
-        print(str(datetime.now())+' '+repo+' ' +'successfully retreived list of forks into forks_df')
-      else:
-          print('skipped converting api response to dataframe')
-          break
-      break     
+        print(str(datetime.now())+' '+repo+' ' +'attempt to retreive a list of forks was unsuccessful, check for errors while converting json response to dataframe/n')
+        print('retrying')
+        continue
+
+  for i in range(0,5):
+    try:
+      #not setting schema as there are many columns
+      #commits_df_job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField("days", bigquery.enums.SqlTypeNames.STRING), ])
+                                                
+      #loading into bq
+      forks_df_job = client.load_table_from_dataframe(forks_df_appended, "idc-external-025.logs.gh_forks") 
+      print('successfully loaded data from forks_df_appended dataframe to bigquery')
+      break
     except:
-      print(str(datetime.now())+' '+repo+' ' +'attempt to retreive a list of forks was unsuccessful, check for errors while converting json response to dataframe/n')
-      print('retrying')
+      print('loading data from forks_df_appended dataframe to bigquery was unsuccessful/n')
+      print('retrying to load into bigquery')
       continue
-
-for i in range(0,5):
-  try:
-    #not setting schema as there are many columns
-    #commits_df_job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField("days", bigquery.enums.SqlTypeNames.STRING), ])
-
-    #loading into bq
-    forks_df_job = client.load_table_from_dataframe(forks_df_appended, "idc-external-025.logs.gh_forks") 
-    print('successfully loaded data from forks_df_appended dataframe to bigquery')
-    break
-  except:
-    print('loading data from forks_df_appended dataframe to bigquery was unsuccessful/n')
-    print('retrying to load into bigquery')
-    continue
