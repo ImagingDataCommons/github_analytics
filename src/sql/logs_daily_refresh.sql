96bff54ab11b5b4326e71f75b1b71ae3556ed0b5
Vamsi Thiriveedhi <115020590+vkt1414@users.noreply.github.com>
2024-01-18 10:57:53 -0500

    change regex expression
    since files are reorganized to folders, the regex needed to be updated as well

--- src/sql/logs_daily_refresh.sql
+++ src/sql/logs_daily_refresh.sql
@@ -9,26 +9,23 @@ insert into logs.log_analysis
 #searching storage access logs for 'GET' (CAN REMOVE THIS FILTER TO CHECK OTHER REQUESTS)
 #requests with non empty cs_object strings,
 WITH storage_access AS
-(SELECT 
+(
+ SELECT 
+ distinct
 time,
 sc_status,
 cs_method,
 sc_bytes,
 cs_bucket,
 cs_object,
-#whenever there is a dash in cs_object,remove only '.dcm' from the end of the string
-#when there is no dash in the sop_instance_uid part of the cs_object,remove  '.dcm' from the sop_instance_uid part of the string
-#when there is no match to above two cases,returns cs_object
-#REGEXP_EXTRACT(cs_uri,r'([^F]+.dcm)') extracts sop_instance_uid from the cs_uri column, and then REGEXP_CONTAINS checks for '-' (dash)
-
-case when not REGEXP_CONTAINS(REGEXP_EXTRACT(cs_object,r'[^/]+dcm$'),'[\\-]') then REGEXP_REPLACE(REGEXP_EXTRACT(cs_object,r'[^/]+dcm$'),'.dcm','')
-     --when not REGEXP_CONTAINS(REGEXP_EXTRACT(cs_uri,r'([^F]+.dcm)'),'[\\-]') then REGEXP_REPLACE(REGEXP_EXTRACT(cs_uri,r'([^F]+.dcm)'),'.dcm','')
-     when REGEXP_CONTAINS(cs_object,r'[\\-]') then REGEXP_EXTRACT(cs_object,r'(.*)\.')
-     else cs_object
-end as extracted_uri
+REGEXP_EXTRACT(cs_object,r'\/(.*)\.dcm') as extracted_uri
 FROM `idc-admin-001.storage_access_prod.bucket_access_logs`  sap
+
 where #SCANS ONLY FROM THE PREVIOUS DAY
-((DATE(time)> date(FORMAT_DATE("%Y-%m-%d", DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)))) and (DATE(time)<= date(FORMAT_DATE("%Y-%m-%d", DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))))) and
+#((DATE(time)> date(FORMAT_DATE("%Y-%m-%d", DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)))) and (DATE(time)<= date(FORMAT_DATE("%Y-%m-%d", DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))))) 
+time < '2024-01-18'
+
+and
 cs_object IS NOT NULL AND 
 cs_object NOT in ('') 
 )
